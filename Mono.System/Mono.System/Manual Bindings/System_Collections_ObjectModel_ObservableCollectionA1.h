f2ed8ff160b82f3c6cd7da00ef88620d0d6968c8
Richard Webb <webby@beardmouse.org.uk>
2017-03-02 17:02:09 +0000

    additional bindings

--- Mono.System/Mono.System/Manual Bindings/System_Collections_ObjectModel_ObservableCollectionA1.h
+++ Mono.System/Mono.System/Manual Bindings/System_Collections_ObjectModel_ObservableCollectionA1.h
@@ -2,7 +2,7 @@
 //
 // Managed class : ObservableCollection`1<T>
 //
-@interface System_Collections_ObjectModel_ObservableCollectionA1 : System_Object <System_Collections_Generic_IEnumerableA1_>
+@interface System_Collections_ObjectModel_ObservableCollectionA1 : System_Collections_ObjectModel_CollectionA1 <System_Collections_Generic_IEnumerableA1_>
 
 - (void)add_withItem:(System_Object *)p1;
 - (BOOL)remove_withItem:(System_Object *)p1;
